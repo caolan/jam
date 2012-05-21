31afab50ad1dcdb59a20239f8eed5290dcfb0c54
Caolan McMahon <caolan@caolanmcmahon.com>
2012-05-21 10:31:39 -0700

    add publish support

--- bin/jam.js
+++ bin/jam.js
@@ -2,6 +2,7 @@
 
 var path = require('path'),
     utils = require('../lib/utils'),
+    jamrc = require('../lib/jamrc'),
     logger = require('../lib/logger'),
     commands = require('../lib/commands');
 
@@ -15,44 +16,45 @@ for (var i = 0; i < args.length; i += 1) {
     }
 }
 
-// TODO: load from .jamrc
-var settings = {};
+jamrc.load(function (err, settings) {
 
-function usage() {
-    console.log('Usage: jam COMMAND [ARGS]');
-    console.log('');
-    console.log('Available commands:');
-    var len = utils.longest(Object.keys(commands));
-    for (var k in commands) {
-        console.log(
-            '  ' + utils.padRight(k, len) + '    ' + commands[k].summary
-        );
+    function usage() {
+        console.log('Usage: jam COMMAND [ARGS]');
+        console.log('');
+        console.log('Available commands:');
+        var len = utils.longest(Object.keys(commands));
+        for (var k in commands) {
+            console.log(
+                '  ' + utils.padRight(k, len) + '    ' + commands[k].summary
+            );
+        }
+        logger.clean_exit = true;
     }
-    logger.clean_exit = true;
-}
 
-if (!args.length) {
-    usage();
-}
-else {
-    var cmd = args.shift();
-    if (cmd === '-h' || cmd === '--help') {
+    if (!args.length) {
         usage();
-    }
-    else if (cmd === '-v' || cmd === '--version') {
-        utils.getJamVersion(function (err, ver) {
-            if (err) {
-                return logger.error(err);
-            }
-            logger.clean_exit = true;
-            console.log(ver);
-        });
-    }
-    else if (cmd in commands) {
-        commands[cmd].run(settings, args, commands);
     }
     else {
-        logger.error('No such command: ' + cmd);
-        usage();
+        var cmd = args.shift();
+        if (cmd === '-h' || cmd === '--help') {
+            usage();
+        }
+        else if (cmd === '-v' || cmd === '--version') {
+            utils.getJamVersion(function (err, ver) {
+                if (err) {
+                    return logger.error(err);
+                }
+                logger.clean_exit = true;
+                console.log(ver);
+            });
+        }
+        else if (cmd in commands) {
+            commands[cmd].run(settings, args, commands);
+        }
+        else {
+            logger.error('No such command: ' + cmd);
+            usage();
+        }
     }
-}
+
+});
