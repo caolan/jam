fbc6f29e706e09dbd56d95313a392fda0db8bd29
Caolan McMahon <caolan@caolanmcmahon.com>
2012-08-10 14:06:10 -0700

    improve api to compile command and add doc comments to index.js

--- index.js
+++ index.js
@@ -8,4 +8,29 @@ var logger = require('./lib/logger');
 logger.level = 'none'
 logger.clean_exit = true;
 
+exports.compile = function (cwd, settings, opt, callback) {
+
+
+/**
+ * Compiles packages into a single file for production use.
+ *
+ * Options:
+ *  cwd             String      working directory (defaults to process.cwd())
+ *  settings        Object      values from jamrc (optional)
+ *  includes        [String]    array of require paths to include in build
+ *  shallowExcludes [String]    exclude these modules (not their dependencies)
+ *  deepExcludes    [String]    exclude these modules AND their dependencies
+ *  output          String      filename to save output to
+ *  pkgdir          String      location of jam packages
+ *  baseurl         String      base url value to pass to requirejs optimizer
+ *  wrap            Bool        wraps output in anonymous function
+ *  almond          Bool        uses almond shim
+ *  verbose         Bool        more verbose output from r.js
+ *  nominify        Bool        don't minify with uglifyjs
+ *  nolicense       Bool        strip license comments
+ *
+ * @param {Object} options
+ * @param {Function} callback(err, build_response)
+ */
+
 exports.compile = require('./lib/commands/compile').compile;
